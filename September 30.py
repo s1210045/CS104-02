ee4bc50d3bf9f2c103ac97c50b5463c6c7e09944
s1210045 <71794419+s1210045@users.noreply.github.com>
2020-10-05 13:39:37 -0400

    Add files via upload

--- September 30.py
+++ September 30.py
@@ -6,16 +6,16 @@ scoreCount = 0
 average = 0.0
 
 #Accept the number of scores to average
-numberOfScores = int(input("Please enter the number of scores you want to average")
+numberOfScores = int(input("Please enter the number of scores you want to average:"))
 
-#Add a loop to make this code repeat until scoreCount = numberOfScores
-score = int(input("Please enter a score: "))
-total = total + score
+
+#Add a loop to make this code repeat until scorecount=numberOfScores
+while (scoreCount < numberOfScores):
+    score = int(input("Please enter a score: "))
+    total= total + score
 scoreCount = scoreCount + 1
 
+#To turn into a string, print average.
+average= total / numberOfScores
+print ("The average of the test score is: " +str(average))
 
-
-
-
-average = total / numberOfScores
-print(average)
