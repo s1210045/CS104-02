bcd6558e7602afb532993ab9f4821d58933fd7b9
s1210045 <71794419+s1210045@users.noreply.github.com>
2020-10-12 16:13:07 -0400

    Update Conditions.py

--- Conditions.py
+++ Conditions.py
@@ -2,12 +2,12 @@
 temp = input("Please enter the current temperature: ")
 
 if temperature >=90:
-	print('Wear Shorts')
+	print("Wear Shorts")
 elif temperature >=70:
-	print('Short sleeves are fine')
+	print("Short sleeves are fine")
 elif temperature >=50:
-	print('Wear a jacket')
+	print("Wear a jacket")
 elif temperature >=32:
-	print('Wear a heavy coat')
+	print("Wear a heavy coat")
 else:
-	print('Stay Inside')
+	print("Stay Inside")
